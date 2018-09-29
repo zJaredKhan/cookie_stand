19ba8c4325b2ef7d48fc374cf989c7c2cb30fbb5
zjaredkhan <jared.khan@outlook.com>
2018-09-29 14:53:44 -0700

    work started on object literals

--- backup.js
+++ backup.js
@@ -10,16 +10,16 @@ var pikePlace = {
 
 pikePlace.calculateCustomersPerHours = function(){
   var randomAmount = Math.floor(Math.random() * (this.max - this.min +1) + this.min);
-  return Math.round(randomAmount);
+  return Math.round(randomAmount * this.avgCookiesPerSale);
 };
+console.log(pikePlace.calculateCustomersPerHours());
 
 pikePlace.calculateCookiesSoldEachHours = function(){
-  for(var i = 0; i < 7; i++){
-    var cookies = Math.round(this.calculateCustomersPerHours() * this.avgCookiesPerSale);
-    this.cookiesSoldEachHour.push(cookies);
+  for(var i = 0; i < 15; i++){
+    this.cookiesSoldEachHour.push(this.pikePlace.calculateCustomersPerHours());
   }
 };
-console.log(pikePlace.cookiesSoldEachHour);
+console.log(pikePlace.calculateCookiesSoldEachHours());
 
 pikePlace.renderHours = function(){
   this.calculateCookiesSoldEachHours();
