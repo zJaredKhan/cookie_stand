8e4fe8ea37bc2a1b93919ea1e6446b70a2fff6f9
zjaredkhan <jared.khan@outlook.com>
2018-09-29 21:46:13 -0700

    got constructor functions to work

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 'use strict';
 
-var pikePlace = {
-  name: 'First and Pike',
-  min: 23,
-  max: 65,
-  avgCookiesPerSale: 6.3,
-  cookiesSoldEachHour: [],
-  cookiesSum: [],
-  hoursOpen: ['6:00 AM - ', '7:00 AM - ', '8:00 AM - ', '9:00 AM - ', '10:00 AM - ', '11:00 AM - ', '12:00 PM - ', '1:00 PM - ', '2:00 PM - ', '3:00 PM - ', '4:00 PM - ', '5:00 PM - ', '6:00 PM - ', '7:00 PM - ', '8:00 PM - ']
-};
+var hoursOpen = ['6:00 AM - ', '7:00 AM - ', '8:00 AM - ', '9:00 AM - ', '10:00 AM - ', '11:00 AM - ', '12:00 PM - ', '1:00 PM - ', '2:00 PM - ', '3:00 PM - ', '4:00 PM - ', '5:00 PM - ', '6:00 PM - ', '7:00 PM - ', '8:00 PM - '];
 
-pikePlace.calculateCustomersPerHours = function(){
+function Franchise(name,min,max,avgCookiesPerSale,hoursOpen){
+  this.name = name;
+  this.min = min;
+  this.max = max;
+  this.avgCookiesPerSale = avgCookiesPerSale;
+  this.cookiesSoldEachHour = [];
+  this.cookiesSum = [],
+  this.hoursOpen = hoursOpen;
+  // this.calculateCookiesSoldEachHours = calculateCookiesSoldEachHours;
+  // this.calculateCustomersPerHours = calculateCustomersPerHours;
+  // this.renderHours = renderHours;
+}
+
+Franchise.prototype.calculateCustomersPerHours = function() {
   var randomAmount = Math.floor(Math.random() * (this.max - this.min +1) + this.min);
   return Math.round(randomAmount);
 };
 
 
-pikePlace.calculateCookiesSoldEachHours = function(){
+Franchise.prototype.calculateCookiesSoldEachHours = function() {
   for(var i = 0; i < 15; i++){
     var cookies = Math.round(this.calculateCustomersPerHours() * this.avgCookiesPerSale);
     this.cookiesSum.push(cookies);
@@ -27,16 +32,19 @@ pikePlace.calculateCookiesSoldEachHours = function(){
   const sum = this.cookiesSum.reduce((total, amount) => total + amount);
   this.cookiesSoldEachHour.push('Total : ' + sum + ' cookies sold');
 };
-console.log(pikePlace.cookiesSoldEachHour);
 
-pikePlace.renderHours = function(){
+var pikePlace = new Franchise('1st and Pike',23,65,6.3,hoursOpen);
+var seaTac = new Franchise('SeaTac Airport', 3,24,1.2,hoursOpen);
+var seaCenter = new Franchise('Seattle Center', 11,38,3.7,hoursOpen);
+var capHill = new Franchise('Capitol Hill', 20,38,2.3,hoursOpen);
+var alki = new Franchise('Alki Beach',2,16,4.6,hoursOpen);
+
+Franchise.prototype.renderHours = function() {
   this.calculateCookiesSoldEachHours();
-  var storesContainer = document.getElementById('pikePlace');
+  var storesContainer = document.getElementById('franchies');
   console.log(storesContainer);
   var headerEl = document.createElement('h2');
-  console.log(headerEl);
   headerEl.textContent = this.name;
-  console.log(headerEl.textContent);
   storesContainer.appendChild(headerEl);
   var ulEl = document.createElement('ul'); //create an element
 
@@ -47,202 +55,10 @@ pikePlace.renderHours = function(){
   }
   storesContainer.appendChild(ulEl);
 };
+
 pikePlace.renderHours();
-
-var seaTac = {
-  name: 'SeaTac Airport',
-  min: 3,
-  max: 24,
-  avgCookiesPerSale: 1.2,
-  cookiesSoldEachHour: [],
-  cookiesSum: [],
-  hoursOpen: ['6:00 AM - ', '7:00 AM - ', '8:00 AM - ', '9:00 AM - ', '10:00 AM - ', '11:00 AM - ', '12:00 PM - ', '1:00 PM - ', '2:00 PM - ', '3:00 PM - ', '4:00 PM - ', '5:00 PM - ', '6:00 PM - ', '7:00 PM - ', '8:00 PM - ']
-};
-
-seaTac.calculateCustomersPerHours = function(){
-  var randomAmount = Math.floor(Math.random() * (this.max - this.min +1) + this.min);
-  return Math.round(randomAmount);
-};
-
-
-seaTac.calculateCookiesSoldEachHours = function(){
-  for(var i = 0; i < 15; i++){
-    var cookies = Math.round(this.calculateCustomersPerHours() * this.avgCookiesPerSale);
-    var hours = this.hoursOpen[i];
-    this.cookiesSum.push(cookies);
-    this.cookiesSoldEachHour.push(hours + cookies);
-  }
-  //reducing (finding sum of array - found at https://medium.freecodecamp.org/reduce-f47a7da511a9 )
-  const sum = this.cookiesSum.reduce((total, amount) => total + amount);
-  this.cookiesSoldEachHour.push('Total : ' + sum + ' cookies sold');
-};
-console.log(seaTac.cookiesSoldEachHour);
-
-seaTac.renderHours = function(){
-  this.calculateCookiesSoldEachHours();
-  var storesContainer = document.getElementById('seaTac');
-  console.log(storesContainer);
-  var headerEl = document.createElement('h2');
-  console.log(headerEl);
-  headerEl.textContent = this.name;
-  console.log(headerEl.textContent);
-  storesContainer.appendChild(headerEl);
-  var ulEl = document.createElement('ul'); //create an element
-
-  for(var i in this.cookiesSoldEachHour){
-    var listItemEl = document.createElement('li');
-    listItemEl.textContent = this.cookiesSoldEachHour[i];
-    ulEl.appendChild(listItemEl);
-  }
-  storesContainer.appendChild(ulEl);
-};
 seaTac.renderHours();
-
-var seaCenter = {
-  name: 'Seattle Center',
-  min: 11,
-  max: 38,
-  avgCookiesPerSale: 3.7,
-  cookiesSoldEachHour: [],
-  cookiesSum: [],
-  hoursOpen: ['6:00 AM - ', '7:00 AM - ', '8:00 AM - ', '9:00 AM - ', '10:00 AM - ', '11:00 AM - ', '12:00 PM - ', '1:00 PM - ', '2:00 PM - ', '3:00 PM - ', '4:00 PM - ', '5:00 PM - ', '6:00 PM - ', '7:00 PM - ', '8:00 PM - ']
-};
-
-seaCenter.calculateCustomersPerHours = function(){
-  var randomAmount = Math.floor(Math.random() * (this.max - this.min +1) + this.min);
-  return Math.round(randomAmount);
-};
-
-
-seaCenter.calculateCookiesSoldEachHours = function(){
-  for(var i = 0; i < 15; i++){
-    var cookies = Math.round(this.calculateCustomersPerHours() * this.avgCookiesPerSale);
-    this.cookiesSum.push(cookies);
-    var hours = this.hoursOpen[i];
-    this.cookiesSoldEachHour.push(hours + cookies);
-  }
-  //reducing (finding sum of array - found at https://medium.freecodecamp.org/reduce-f47a7da511a9 )
-  const sum = this.cookiesSum.reduce((total, amount) => total + amount);
-  this.cookiesSoldEachHour.push('Total : ' + sum + ' cookies sold');
-};
-console.log(seaCenter.cookiesSoldEachHour);
-
-seaCenter.renderHours = function(){
-  this.calculateCookiesSoldEachHours();
-  var storesContainer = document.getElementById('seaCenter');
-  console.log(storesContainer);
-  var headerEl = document.createElement('h2');
-  console.log(headerEl);
-  headerEl.textContent = this.name;
-  console.log(headerEl.textContent);
-  storesContainer.appendChild(headerEl);
-  var ulEl = document.createElement('ul'); //create an element
-
-  for(var i in this.cookiesSoldEachHour){
-    var listItemEl = document.createElement('li');
-    listItemEl.textContent = this.cookiesSoldEachHour[i];
-    ulEl.appendChild(listItemEl);
-  }
-  storesContainer.appendChild(ulEl);
-};
 seaCenter.renderHours();
-
-var capHill = {
-  name: 'Capitol Hill',
-  min: 11,
-  max: 38,
-  avgCookiesPerSale: 3.7,
-  cookiesSoldEachHour: [],
-  cookiesSum: [],
-  hoursOpen: ['6:00 AM - ', '7:00 AM - ', '8:00 AM - ', '9:00 AM - ', '10:00 AM - ', '11:00 AM - ', '12:00 PM - ', '1:00 PM - ', '2:00 PM - ', '3:00 PM - ', '4:00 PM - ', '5:00 PM - ', '6:00 PM - ', '7:00 PM - ', '8:00 PM - ']
-};
-
-capHill.calculateCustomersPerHours = function(){
-  var randomAmount = Math.floor(Math.random() * (this.max - this.min +1) + this.min);
-  return Math.round(randomAmount);
-};
-
-
-capHill.calculateCookiesSoldEachHours = function(){
-  for(var i = 0; i < 15; i++){
-    var cookies = Math.round(this.calculateCustomersPerHours() * this.avgCookiesPerSale);
-    this.cookiesSum.push(cookies);
-    var hours = this.hoursOpen[i];
-    this.cookiesSoldEachHour.push(hours + cookies);
-  }
-  //reducing (finding sum of array - found at https://medium.freecodecamp.org/reduce-f47a7da511a9 )
-  const sum = this.cookiesSum.reduce((total, amount) => total + amount);
-  this.cookiesSoldEachHour.push('Total : ' + sum + ' cookies sold');
-};
-console.log(capHill.cookiesSoldEachHour);
-
-capHill.renderHours = function(){
-  this.calculateCookiesSoldEachHours();
-  var storesContainer = document.getElementById('capHill');
-  console.log(storesContainer);
-  var headerEl = document.createElement('h2');
-  console.log(headerEl);
-  headerEl.textContent = this.name;
-  console.log(headerEl.textContent);
-  storesContainer.appendChild(headerEl);
-  var ulEl = document.createElement('ul'); //create an element
-
-  for(var i in this.cookiesSoldEachHour){
-    var listItemEl = document.createElement('li');
-    listItemEl.textContent = this.cookiesSoldEachHour[i];
-    ulEl.appendChild(listItemEl);
-  }
-  storesContainer.appendChild(ulEl);
-};
 capHill.renderHours();
-
-var alki = {
-  name: 'Alki Beach',
-  min: 11,
-  max: 38,
-  avgCookiesPerSale: 3.7,
-  cookiesSoldEachHour: [],
-  cookiesSum: [],
-  hoursOpen: ['6:00 AM - ', '7:00 AM - ', '8:00 AM - ', '9:00 AM - ', '10:00 AM - ', '11:00 AM - ', '12:00 PM - ', '1:00 PM - ', '2:00 PM - ', '3:00 PM - ', '4:00 PM - ', '5:00 PM - ', '6:00 PM - ', '7:00 PM - ', '8:00 PM - ']
-};
-
-alki.calculateCustomersPerHours = function(){
-  var randomAmount = Math.floor(Math.random() * (this.max - this.min +1) + this.min);
-  return Math.round(randomAmount);
-};
-
-
-alki.calculateCookiesSoldEachHours = function(){
-  for(var i = 0; i < 15; i++){
-    var cookies = Math.round(this.calculateCustomersPerHours() * this.avgCookiesPerSale);
-    this.cookiesSum.push(cookies);
-    var hours = this.hoursOpen[i];
-    this.cookiesSoldEachHour.push(hours + cookies);
-  }
-  //reducing (finding sum of array - found at https://medium.freecodecamp.org/reduce-f47a7da511a9 )
-  const sum = this.cookiesSum.reduce((total, amount) => total + amount);
-  this.cookiesSoldEachHour.push('Total : ' + sum + ' cookies sold');
-};
-console.log(alki.cookiesSoldEachHour);
-
-alki.renderHours = function(){
-  this.calculateCookiesSoldEachHours();
-  var storesContainer = document.getElementById('alki');
-  console.log(storesContainer);
-  var headerEl = document.createElement('h2');
-  console.log(headerEl);
-  headerEl.textContent = this.name;
-  console.log(headerEl.textContent);
-  storesContainer.appendChild(headerEl);
-  var ulEl = document.createElement('ul'); //create an element
-
-  for(var i in this.cookiesSoldEachHour){
-    var listItemEl = document.createElement('li');
-    listItemEl.textContent = this.cookiesSoldEachHour[i];
-    ulEl.appendChild(listItemEl);
-  }
-  storesContainer.appendChild(ulEl);
-};
 alki.renderHours();
-
 
