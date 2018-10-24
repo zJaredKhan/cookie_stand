e62654f844b605e1f0e538bbedc09cd5229713ac
zjaredkhan <jared.khan@outlook.com>
2018-10-24 09:44:44 -0700

    getting started again

--- backup.js
+++ backup.js
@@ -33,11 +33,13 @@ Franchise.prototype.calculateCookiesSoldEachHours = function() {
   this.cookiesSoldEachHour.push('Total : ' + sum + ' cookies sold');
 };
 
+//constructing the franchies
 var pikePlace = new Franchise('1st and Pike',23,65,6.3,hoursOpen);
 var seaTac = new Franchise('SeaTac Airport', 3,24,1.2,hoursOpen);
 var seaCenter = new Franchise('Seattle Center', 11,38,3.7,hoursOpen);
 var capHill = new Franchise('Capitol Hill', 20,38,2.3,hoursOpen);
 var alki = new Franchise('Alki Beach',2,16,4.6,hoursOpen);
+var storeNames = [pikePlace, seaTac,seaCenter, capHill, alki,];
 
 Franchise.prototype.renderHours = function() {
   this.calculateCookiesSoldEachHours();
@@ -56,9 +58,38 @@ Franchise.prototype.renderHours = function() {
   storesContainer.appendChild(ulEl);
 };
 
-pikePlace.renderHours();
-seaTac.renderHours();
-seaCenter.renderHours();
-capHill.renderHours();
-alki.renderHours();
+// pikePlace.renderHours();
+// seaTac.renderHours();
+// seaCenter.renderHours();
+// capHill.renderHours();
+// alki.renderHours();
 
+//create table
+var rowsTotal = storeNames.length;
+var cellsTotal = hoursOpen.length;
+
+
+function createTable() {
+  var table = document.createElement('table');
+  function createTableHead() {
+    
+  }
+for (var i = 1; i < 4; i++){
+    var tr = document.createElement('tr');   
+
+    var td1 = document.createElement('td');
+    var td2 = document.createElement('td');
+
+    var text1 = document.createTextNode('Text1');
+    var text2 = document.createTextNode('Text2');
+
+    td1.appendChild(text1);
+    td2.appendChild(text2);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+
+    table.appendChild(tr);
+}
+document.body.appendChild(table);
+ 
+createTable();
