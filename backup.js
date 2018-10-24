435ed2d192841d92c408327a2a6b3c37034e33da
zjaredkhan <jared.khan@outlook.com>
2018-10-24 11:34:36 -0700

    still working on getting the table to render

--- backup.js
+++ backup.js
@@ -4,14 +4,6 @@
 var hoursOpen = ['6:00 AM - ', '7:00 AM - ', '8:00 AM - ', '9:00 AM - ', '10:00 AM - ', '11:00 AM - ', '12:00 PM - ', '1:00 PM - ', '2:00 PM - ', '3:00 PM - ', '4:00 PM - ', '5:00 PM - ', '6:00 PM - ', '7:00 PM - ', '8:00 PM - '];
 var franchiseLocations = [];
 
-//constructing the franchies
-var pikePlace = franchiseLocations.push (new Franchise('1st and Pike',23,65,6.3,hoursOpen));
-var seaTac = franchiseLocations.push(new Franchise('SeaTac Airport', 3,24,1.2,hoursOpen));
-var seaCenter = franchiseLocations.push(new Franchise('Seattle Center', 11,38,3.7,hoursOpen));
-var capHill = franchiseLocations.push(new Franchise('Capitol Hill', 20,38,2.3,hoursOpen));
-var alki = franchiseLocations.push(new Franchise('Alki Beach',2,16,4.6,hoursOpen));
-
-
 //franchise constructor
 function Franchise(name,min,max,avgCookiesPerSale,hoursOpen){
   this.name = name;
@@ -23,6 +15,15 @@ function Franchise(name,min,max,avgCookiesPerSale,hoursOpen){
   this.hoursOpen = hoursOpen;
   franchiseLocations.push(this);
 
+//constructing the franchies
+var pikePlace = franchiseLocations.push (new Franchise('1st and Pike',23,65,6.3,hoursOpen));
+var seaTac = franchiseLocations.push(new Franchise('SeaTac Airport', 3,24,1.2,hoursOpen));
+var seaCenter = franchiseLocations.push(new Franchise('Seattle Center', 11,38,3.7,hoursOpen));
+var capHill = franchiseLocations.push(new Franchise('Capitol Hill', 20,38,2.3,hoursOpen));
+var alki = franchiseLocations.push(new Franchise('Alki Beach',2,16,4.6,hoursOpen));
+
+
+
   // this.calculateCookiesSoldEachHours = calculateCookiesSoldEachHours;
   // this.calculateCustomersPerHours = calculateCustomersPerHours;
   // this.renderHours = renderHours;
@@ -30,18 +31,18 @@ function Franchise(name,min,max,avgCookiesPerSale,hoursOpen){
 }
 
 Franchise.prototype.calculateCustomersPerHours = function() {
-  var randomAmount = Math.floor(Math.random() * (this.max - this.min +1) + this.min);
-  return Math.round(randomAmount);
+  return Math.floor(Math.random() * (this.max - this.min +1) + this.min);
 };
+
 
 
 Franchise.prototype.calculateCookiesSoldEachHours = function() {
   for(var i = 0; i < 15; i++){
-    var cookies = Math.round(this.calculateCustomersPerHours() * this.avgCookiesPerSale);
-    this.cookiesSum.push(cookies);
-    var hours = this.hoursOpen[i];
-    this.cookiesSoldEachHour.push(hours + cookies);
+    var cookies = Math.floor(this.calculateCustomersPerHours() * this.avgCookiesPerSale);
+    this.cookiesSoldEachHour.push(cookies);
+    this.cookiesSum += cookies;
   }
+
   //reducing (finding sum of array - found at https://medium.freecodecamp.org/reduce-f47a7da511a9 )
   const sum = this.cookiesSum.reduce((total, amount) => total + amount);
   this.cookiesSoldEachHour.push('Total : ' + sum + ' cookies sold');
@@ -84,7 +85,7 @@ function generateTableHeading(){
   }
 
   var tdTotal = document.createElement('th');
-  tdTotal.textContent = "test";
+  tdTotal.textContent = 'this.cookiesSum';
   tdTotal.innerHTML = "Total";
   row.appendChild(tdTotal);
   storeSection.appendChild(row);
@@ -92,6 +93,7 @@ function generateTableHeading(){
 
 generateTableHeading();
 
-franchiseLocations.forEach(function(name){
-  name.render();
+franchiseLocations.forEach(function(store){
+  store.cookiesSoldEachHour();
+  store.render();
 });
